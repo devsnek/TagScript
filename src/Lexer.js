9ad18bf490894c5e090c9c5773ab5c5a4f17cf6c
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-10-22 12:54:19 -0500

    fuck you dougley

--- src/Lexer.js
+++ src/Lexer.js
@@ -1,9 +1,13 @@
 const chevrotain = require('chevrotain');
 
 if (!Object.values) {
-  Object.values = (o) => {
-    return Object.keys(o).map(k => o[k]);
-  }
+  const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+  const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+  const concat = Function.bind.call(Function.call, Array.prototype.concat);
+  const keys = Reflect.ownKeys;
+  Object.values = function values (O) {
+    return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+  };
 }
 
 Object.defineProperty(Array.prototype, 'last', { // eslint-disable-line
